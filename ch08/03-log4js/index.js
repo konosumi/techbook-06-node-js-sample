9d41d7dabd78ce5fd53d7583952d19077e5e5a43
konosumi <36559308+konosumi@users.noreply.github.com>
2019-08-20 02:40:55 +0900

    Update index.js

--- ch08/03-log4js/index.js
+++ ch08/03-log4js/index.js
@@ -2,7 +2,7 @@
 const log4js = require('log4js');
 log4js.configure({
   appenders: { cheese: { type: 'file', filename: 'cheese.log' } },
-  // ログに出力するエラーレベルは、本番環境と開発環境で調整したりして使います
+  // ログに出力するエラーレベルは、本番環境と開発環境で環境毎に調整して使います
   // categories: { default: { appenders: ['cheese'], level: 'error' } }
   categories: { default: { appenders: ['cheese'], level: 'debug' } }
 });
