6146f998f0079cad5abdda32ca2c7e87bc65f89f
konosumi <36559308+konosumi@users.noreply.github.com>
2019-08-20 23:09:45 +0900

    Create index.js

---
+++ ch09/03-this-probrem/index.js
@@ -0,0 +1,28 @@
+function Person() {
+    this.age = 0;
+
+    const growUp = function growUp() {
+      // functionのthisは自分自身(growUp function)を指すためNaNになる
+      this.age++;
+  
+      console.log('function this age:', this.age);
+    };
+
+    // 普通に実行すると、関数内のthisは関数自身となる
+    // function this age: NaN
+    setTimeout(growUp, 500);
+
+    // thisを現在のオブジェクト(Person)に束縛する
+    // function this age: 1
+    setTimeout(growUp.bind(this), 600);
+   
+    setTimeout(() => {
+      // アロー関数はthisを持たないので、Personのthisが参照される
+      this.age++;
+  
+      // arrow func age: 2
+      console.log('arrow func age:', this.age);
+    }, 700);
+  }
+  
+  const p = new Person();
